df2f19836603d872d80c7fe6bee9544e168600d6
Thibault BERRIDO <thberrid@e1r3p7.42.fr>
2019-05-30 18:26:50 +0200

    gal improvment

--- get_next_line.c
+++ get_next_line.c
@@ -5,129 +5,90 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: thberrid <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/12/10 14:59:54 by thberrid          #+#    #+#             */
-/*   Updated: 2019/05/25 17:06:54 by thberrid         ###   ########.fr       */
+/*   Created: 2019/05/30 12:42:02 by thberrid          #+#    #+#             */
+/*   Updated: 2019/05/30 18:24:30 by thberrid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include <libft.h>
+#include <get_next_line.h>
 
-static int	ft_bufffree(int fd, t_list **list, int ret)
+/*
+static int	line_concat(char **line, int *line_len, t_buf *buffer)
 {
-	t_list	*head;
-	t_list	*previous;
-	t_buff	*buff;
+	char	*endl_pos;
+	int		rest_len;
 
-	head = *list;
-	previous = NULL;
-	while (head)
-	{
-		buff = head->content;
-		if (buff->fd == fd)
-		{
-			if (previous)
-				previous->next = head->next;
-			else
-				*list = head->next;
-			ft_memdel((void **)&(buff->data));
-			ft_memdel((void **)&buff);
-			ft_memdel((void **)&head);
-			return (ret);
-		}
-		previous = head;
-		head = head->next;
-	}
-	return (GNL_ERROR);
+	rest_len = buffer->len;
+	endl_pos = ft_memchr(buffer->arr, '\n', rest_len);
+	if (endl_pos)
+		rest_len = endl_pos - buffer->arr;
+	*line = ft_memrealloc(*line, *line_len, *line_len + rest_len);
+	if (!*line)
+		return (FT_ERROR);
+	ft_memmove(*line + *line_len, buffer->arr, rest_len);
+	*line_len += rest_len;
+	return (1);
 }
 
-static int	ft_buffget(int fd, t_list **list, t_buff **buff)
+static int	buffer_update(t_buf *buffer)
 {
-	t_list	*head;
+	char	*endl_pos;
+	size_t	blank_len;
 
-	head = *list;
-	while (head)
+	endl_pos = ft_memchr(buffer->arr, '\n', buffer->len);
+	if (!endl_pos)
 	{
-		*buff = head->content;
-		if ((*buff)->fd == fd)
-			return (GNL_OK);
-		head = head->next;
+		ft_bzero(buffer->arr, BUFF_SIZE);
+		buffer->len = 0;
+		return (0);
 	}
-	if (!(*buff = (t_buff *)malloc(sizeof(t_buff))))
-		return (0);
-	(*buff)->fd = fd;
-	(*buff)->size = 0;
-	(*buff)->cmd = GNL_READ;
-	if (!((*buff)->data = ft_strnew(BUFF_SIZE)))
-		return (0);
-	if (!(head = ft_lstnew(*buff, sizeof(t_buff))))
-		return (0);
-	ft_memdel((void **)buff);
-	ft_lstadd(list, head);
-	return (ft_buffget(fd, list, buff));
+	blank_len = endl_pos - buffer->arr + 1;
+	buffer->len = BUFF_SIZE - blank_len;
+	ft_putnbr(buffer->len);
+	ft_putendl("");
+	ft_putnbr(blank_len);
+	ft_putendl("");
+	ft_memmove(buffer->arr, endl_pos + 1, buffer->len);
+	ft_bzero(buffer->arr + buffer->len, blank_len);
+	return (1);
 }
 
-static int	ft_bufflock(t_buff **buff, char **line)
+static int	transfer_toline(char **line, int *line_len, t_buf *buffer)
 {
-	size_t	section_len;
-	char	*endl_position;
-	size_t	new_line_len;
-
-	endl_position = NULL;
-	section_len = BUFF_SIZE;
-	if ((endl_position = ft_strchr((*buff)->data, GNL_ENDL)))
-	{
-		(*buff)->cmd = GNL_SEND;
-		section_len = endl_position - (*buff)->data;
-	}
-	new_line_len = ft_strlen(*line) + section_len + 1;
-	if (!(*line = (char *)ft_memrealloc(*line, new_line_len)))
+	if (!buffer->len)
 		return (0);
-	ft_strncat(*line, (*buff)->data, section_len);
-	if (endl_position)
-		ft_strncpy((*buff)->data, endl_position + 1, BUFF_SIZE);
-	else
-		ft_bzero((*buff)->data, BUFF_SIZE + 1);
-	(*buff)->size = BUFF_SIZE - section_len;
-	return (GNL_OK);
-}
-
-static int	ft_lineread(int fd, char **line, t_list **list, t_buff **buff)
-{
-	while ((*buff)->cmd == GNL_READ)
-	{
-		if (!(*buff)->size)
-		{
-			(*buff)->size = read(fd, (*buff)->data, BUFF_SIZE);
-			if ((*buff)->size <= 0)
-			{
-				if (!(*buff)->size && ft_strlen(*line))
-					return (GNL_OK);
-				return (ft_bufffree(fd, list, (*buff)->size));
-			}
-		}
-		if (!ft_bufflock(buff, line))
-			return (ft_bufffree(fd, list, GNL_ERROR));
-	}
-	if ((*buff)->cmd == GNL_SEND)
-	{
-		(*buff)->cmd = GNL_READ;
-		return (GNL_OK);
-	}
-	return (ft_bufffree(fd, list, GNL_EOF));
+	if (line_concat(line, line_len, buffer) < 0)
+		return (FT_ERROR);
+	if (buffer_update(buffer))
+		return (1);
+	return (0);
 }
 
 int			get_next_line(const int fd, char **line)
 {
-	static t_list	*list;
-	t_buff			*buff;
-
-	if (!line || fd < 0 || !BUFF_SIZE)
-		return (ft_bufffree(fd, &list, GNL_ERROR));
-	if (*line)
-		ft_strdel(line);
-	if (!(*line = ft_strnew(BUFF_SIZE)))
-		return (ft_bufffree(fd, &list, GNL_ERROR));
-	if (!ft_buffget(fd, &list, &buff))
-		return (ft_bufffree(fd, &list, GNL_ERROR));
-	return (ft_lineread(fd, line, &list, &buff));
+	static t_buf	buffer;
+	int				newline;
+	int				read_bytes;
+	int				line_len;
+	
+	if (fd < 1 || !line)
+		return (FT_ERROR);	
+	ft_strdel(line);
+	line_len = 0;
+	if ((newline = transfer_toline(line, &line_len, &buffer)) != 0)
+		return (newline);
+	while ((read_bytes = read(fd, buffer.arr, BUFF_SIZE)))
+	{
+		if (read_bytes < 0)
+			return (FT_ERROR);
+		buffer.len = read_bytes;
+		if ((newline = transfer_toline(line, &line_len, &buffer) != 0))
+			return (newline);
+		if (read_bytes < BUFF_SIZE)
+			break ;
+	}
+	return ((read_bytes > 0));
 }
+*/
+
