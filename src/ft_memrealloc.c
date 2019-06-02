9ebff56e49917e41e18bb3ee1bc212d55c435b49
Thibault BERRIDO <thberrid@e1r2p15.42.fr>
2019-06-02 16:20:54 +0200

    protect malloc

--- src/ft_memrealloc.c
+++ src/ft_memrealloc.c
@@ -6,7 +6,7 @@
 /*   By: thberrid <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/10 15:02:51 by thberrid          #+#    #+#             */
-/*   Updated: 2019/05/30 14:55:51 by thberrid         ###   ########.fr       */
+/*   Updated: 2019/05/31 22:28:25 by thberrid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,10 @@ void	*ft_memrealloc(void *src, size_t old_size, size_t new_size)
 
 	dst = ft_memalloc(new_size);
 	if (!dst)
+	{
+		ft_memdel(&src);
 		return (NULL);
+	}
 	ft_bzero(dst, new_size);
 	ft_memcpy(dst, src, old_size);
 	ft_memdel(&src);
